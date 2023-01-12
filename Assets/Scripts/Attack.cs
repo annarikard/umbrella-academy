5856d2684869056b2eb176486fa160634dd26655
Anna Rikardsson <rikardsson.anna@gmail.com>
2023-01-12 15:23:11 +0100

    double tap works bishesssss

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -24,26 +24,34 @@ public class Attack : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown("l") && !attacking)
+    {		
+		Debug.Log("I AM NOT RUNNNING!!!!!!!!");
+
+		Debug.Log(attacking);
+        /*if (!attacking)
         {
-            if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Fold"))
-            {
-                attacking = true;
+		Debug.Log("entering");
+			if (Input.touchCount > 0) {
+				Debug.Log("i have touchcount");
+				Debug.Log(IsDoubleTap());
+				Debug.Log("I am in first if");
+            	if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Fold"))
+            	{
+					Debug.Log("I am in second if");
+                	attacking = true;
 
-                _animator.SetTrigger("Attack");
-
-                StartCoroutine(SpawnBullet());
+                	_animator.SetTrigger("Attack");
+					Debug.Log("I am starting a coroutine");
+                	StartCoroutine(SpawnBullet());
                 
-            }
-        }
+            	}
+			}
+        }*/
     }
 
     IEnumerator SpawnBullet()
     {
-
         GameObject bulletSpawner = GameObject.Find("BulletSpawner");
-
 
         UnityEngine.Quaternion newRotation = UnityEngine.Quaternion.identity;
         newRotation.Set(bulletSpawner.transform.rotation.x, bulletSpawner.transform.rotation.y, bulletSpawner.transform.rotation.y, newRotation.w);
@@ -53,4 +61,50 @@ public class Attack : MonoBehaviour
         Instantiate(bullet, bulletSpawner.transform.position, newRotation);
         attacking = false;
     }
+
+	public static bool IsDoubleTap(){
+         bool result = false;
+         float MaxTimeWait = 3;
+         float VariancePosition = 3;
+		float tapCount = 0;
+		float touchTime = 0;
+		float touchEndTime = 0;
+ 
+		Debug.Log("in dt");
+		Debug.Log(Input.touchCount);
+         if(Input.touchCount == 1)
+        {
+			Debug.Log("touchcount");
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                tapCount++;
+				Debug.Log("inc tapcount");
+                touchTime = Time.time;
+            }
+ 
+            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                if (tapCount == 1)
+                {
+                    touchEndTime = Time.time;
+ 
+                    if (touchEndTime - touchTime < 1.2f)
+                    {
+                        Debug.Log("Single Tap");
+                    }
+                }
+ 
+                if (tapCount == 2)
+                {
+                    Debug.Log("Double Tap");
+					result = true;
+                }
+            }
+       
+        }
+
+		Debug.Log("Result from IsDoubleTap:");
+		Debug.Log(result);
+         return result;
+     }
 }
--- Assets/Scripts/CloseOpen.cs
+++ Assets/Scripts/CloseOpen.cs
@@ -4,42 +4,94 @@ using UnityEngine;
 
 public class CloseOpen : MonoBehaviour
 {
-    private Animator _animator;
+   private Animator _animator;
+   public GameObject bullet;
+   public bool attacking;
+    public float attackSpeedModifier;
+    // Completely arbitrary, must be set to 1.2/attackSpeedModifier
+    private float attackSpeedSeconds;
+
     private int i  = 0;
+    private float doubleTapThreshold = 0.3f;
+    private int tapCount;
     // Start is called before the first frame update
     void Start()
     {
+        attacking = false;
         _animator = gameObject.GetComponent<Animator>();
+        _animator.speed = attackSpeedModifier;
+        attackSpeedSeconds = 1.2f/attackSpeedModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the screen is being touched
-        if (Input.touchCount > 0)
+        if (Input.touchCount == 1)
         {
-            // Get the first touch
-            Touch touch = Input.GetTouch(0);
-
-            // If the touch just started, open or close the umbrella
-            if (touch.phase == TouchPhase.Began)
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
-                UmbrellaCollisionHandler umbrellaCollisionHandler = (UmbrellaCollisionHandler)GetComponent("UmbrellaCollisionHandler");
-                if (i % 2 == 0)
-                {
-                    if (_animator != null)
-                    {
-                        _animator.SetTrigger("Fold");
-                        umbrellaCollisionHandler.isFolded = true;
-                    }
-                }
-                else
-                {
-                    _animator.SetTrigger("Unfold");
-                    umbrellaCollisionHandler.isFolded = false;
-                }
-                i++;
+                tapCount++;
+                StartCoroutine(SingleOrDoubleTap());
             }
         }
+    }
+
+    public void OpenClose() {
+        UmbrellaCollisionHandler umbrellaCollisionHandler = (UmbrellaCollisionHandler) GetComponent("UmbrellaCollisionHandler");
+        if (i % 2 == 0)
+        {
+            if (_animator != null)
+            {
+                _animator.SetTrigger("Fold");
+                umbrellaCollisionHandler.isFolded = true;
+            }
+        }
+        else
+        {
+            _animator.SetTrigger("Unfold");
+            umbrellaCollisionHandler.isFolded = false;
+        }
+        i++;
+    }
+
+    public void Attack() {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Fold"))
+        {
+            attacking = true;
+            _animator.SetTrigger("Attack");
+            StartCoroutine(SpawnBullet());        
+        }
+    }
+
+    IEnumerator SpawnBullet()
+    {
+        GameObject bulletSpawner = GameObject.Find("BulletSpawner");
+
+        UnityEngine.Quaternion newRotation = UnityEngine.Quaternion.identity;
+        newRotation.Set(bulletSpawner.transform.rotation.x, bulletSpawner.transform.rotation.y, bulletSpawner.transform.rotation.y, newRotation.w);
+
+        // Swith to events rather than a hard-coded time value
+        yield return new WaitForSeconds(attackSpeedSeconds);
+        Instantiate(bullet, bulletSpawner.transform.position, newRotation);
+        attacking = false;
+    }
+
+    IEnumerator SingleOrDoubleTap()
+    {
+        yield return new WaitForSeconds(doubleTapThreshold);
+ 
+        if (tapCount == 1)
+        {
+            Debug.Log("SingleTap");
+            OpenClose();
+            tapCount = 0;
+        }
+        else if (tapCount == 2)
+        {
+            Debug.Log("Double Tap");
+            Attack();
+            tapCount = 0;
+        }
+ 
     }
 }
